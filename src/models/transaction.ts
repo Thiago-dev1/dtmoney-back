bd66d50af6d81536b224444c90b0fbeb654ac991
Thiago <tiagohenrriquebuenodeoliveira@gmaiil.com>
2024-01-14 18:04:47 -0300

    Add transaction model and update list transaction controller

---
+++ src/models/transaction.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import { Transaction } from "../modules/transactions/repositories/ITransationRepository";
+
+// terá que ser criado um schema para o mongoose, com os mesmos campos do prisma
+const schema = new mongoose.Schema({
+    title: String,
+    amount: Number,
+    category: String,
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    type: String
+})
+
+// o mongoose irá criar uma collection com o nome transactions
+const TransactionModel = mongoose.model<Transaction>('Transaction', schema)
+
+export { TransactionModel };
+
--- src/modules/transactions/useCase/listTransaction/ListTransactionController.ts
+++ src/modules/transactions/useCase/listTransaction/ListTransactionController.ts
@@ -1,5 +1,5 @@
-import { container } from "tsyringe"
 import { Request, Response } from "express"
+import { container } from "tsyringe"
 
 import { ListTransactionUseCase } from "./ListTransactionUseCase"
 
@@ -7,29 +7,11 @@ class ListTransactionController {
 
     async handle(request: Request, response: Response): Promise<Response> {
 
-        const type = request.query.type
-        const take = request.query.take
-        // const skip = request.query.skip
-        const page = request.query.page
+        const type = request.query.type 
+        const take = request.query.take ? Number(request.query.take) : 10
+        let skip = request.query.skip ? Number(request.query.skip ): 0
 
         
-
-        if(Number(page) > 1 ) {
-            const skip = (Number(page) -1 ) * Number(take)
-
-            const listTransactionController = container.resolve(ListTransactionUseCase)
-
-            const all = await listTransactionController.execute({
-                type: type as string, 
-                take: Number(take), 
-                skip: Number(skip)
-            })
-    
-            
-            return response.status(200).json(all)
-        }
-
-        const skip = 0
         
         const listTransactionController = container.resolve(ListTransactionUseCase)
 
@@ -45,3 +27,4 @@ class ListTransactionController {
 }
 
 export { ListTransactionController }
+
