d34499c372c53d8c269ece0427ca13b65ebdf063
Thiago <tiagohenrriquebuenodeoliveira@gmail.com>
2024-02-27 19:50:32 -0300

    Atualiza o caso de uso UpdateSheetsUseCase para lidar com erros e adicionar logs

--- src/modules/transactions/useCase/updateSheets/UpdateSheetsUseCase.ts
+++ src/modules/transactions/useCase/updateSheets/UpdateSheetsUseCase.ts
@@ -19,36 +19,51 @@ class UpdateSheetsUseCase {
 		tabName: string,
 		tableShort = { collum: 'createAt', direction: 'asc' },
 	) {
-		if (!spreadsheetId || !tabName) {
-			console.error(
-				`[UpdateSheetsUseCase] -> SpreadsheetId e tabName são obrigatórios ${userId}`,
+		try {
+			if (!spreadsheetId || !tabName) {
+				console.error(
+					`[UpdateSheetsUseCase] -> SpreadsheetId e tabName são obrigatórios ${userId}`,
+				)
+				throw new Error('SpreadsheetId e tabName são obrigatórios')
+			}
+
+			const transactions = await this.transactionRepository.list(
+				userId,
+				tableShort,
 			)
-			throw new Error('SpreadsheetId e tabName são obrigatórios')
+
+			/**
+			 * Formatar as transações para o formato do google sheets
+			 * primeira linha: Titulo, Valor, Categoria, Data, Tipo
+			 */
+
+			const data = transactions.all.map((transaction) => {
+				return [
+					transaction.title,
+					transaction.amount,
+					transaction.category.title,
+					dateUtil.formatDate(transaction.createdAt),
+					transaction.type,
+				]
+			})
+
+			const range = 'A2:E' + (data.length + 1)
+
+			await this.googleSheets.writeSheet(
+				data,
+				spreadsheetId,
+				range,
+				tabName,
+			)
+
+			return data
+		} catch (error) {
+			console.error(
+				`[UpdateSheetsUseCase] -> Erro ao atualizar planilha ${userId}`,
+				error,
+			)
+			throw new Error('Erro ao atualizar planilha')
 		}
-
-		const transactions = await this.transactionRepository.list(
-			userId,
-			tableShort,
-		)
-
-		/**
-		 * Formatar as transações para o formato do google sheets
-		 * primeira linha: Titulo, Valor, Categoria, Data, Tipo
-		 */
-
-		const data = transactions.all.map((transaction) => {
-			return [
-				transaction.title,
-				transaction.amount,
-				transaction.category.title,
-				dateUtil.formatDate(transaction.createdAt),
-				transaction.type,
-			]
-		})
-
-		const range = 'A2:E' + (data.length + 1)
-
-		await this.googleSheets.writeSheet(data, spreadsheetId, range, tabName)
 	}
 }
 
